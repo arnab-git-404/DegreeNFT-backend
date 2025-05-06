6503c43a5db473af801207adfab711b5e63c8771
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2025-05-07 01:14:27 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -6,7 +6,7 @@ const connectDatabase = async () => {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'DegreeNft'
     });
-    console.log('MongoDB connected successfully');
+    console.log('MongoDB connected successfully.');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1); // Exit with failure
